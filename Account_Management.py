512fe37336483dc70993a380df70e3a50e299ff2
Brian Quesenberry <Brian.Quesenberry21@gmail.com>
2020-09-01 15:48:06 -0700

    First draft account management

--- Account_Management.py
+++ Account_Management.py
@@ -1,19 +1,35 @@
 # class to modify and create accounts
 # 9/1/2020
 
+import os
 
 class Login():
     """ Log in """
+
     def __init__(self, username, password):
+
         self.user = username
         self.password = password
-        self.test()
 
-    def test(self):
-        print(self.user)
-        print(self.password)
+    def CheckLogin(self):
+        for line in open(os.path.join(os.getcwd(), 'User_Data.txt')):
+            if self.user == (line.split(':'))[0]:
+                if self.password == (line.split(':'))[1]:
+                    return True
+                return False
+            return False
 
-    # create
-    # modify
-    # delete
-    pass
+class SignUp():
+
+    def __init__(self, fname, lname, username, password):
+        self.username = username
+        self.password = password
+        self.fname = fname
+        self.lname = lname
+
+    def CheckSignUp(self):
+        for line in open(os.path.join(os.getcwd(), 'User_Data.txt')):
+            if self.user == (line.split(':'))[0]:
+                if self.password == (line.split(':'))[1]:
+                    return False
+                return False
