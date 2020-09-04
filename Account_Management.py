538ad0b68aab58036ee991cd467296c810669753
Brian Quesenberry <Brian.Quesenberry21@gmail.com>
2020-09-03 18:33:25 -0700

    Added comments

--- Account_Management.py
+++ Account_Management.py
@@ -3,27 +3,24 @@
 
 import os
 
-
-class Login():
-    """ Log in """
+class Login(): #Logs in users who already have an account
 
     def __init__(self, username, password):
-
         self.user = username
         self.password = password
 
-    def CheckLogin(self):
+    def CheckLogin(self): #Checks if login information is correct
         userdata = open(os.path.join(os.getcwd(), 'User_Data.txt'))
-        for line in open(os.path.join(os.getcwd(), 'User_Data.txt')):
-            if self.user == (line.split(':'))[0]:
-                if self.password == (line.split(':'))[1]:
+        for line in userdata:
+            if self.user == (line.split(':'))[0]: #Checks if username is correct
+                if self.password == (line.split(':'))[1]: #Checks if password is also correct
                     userdata.close()
-                    return True
+                    return True #Returns true if username and password are correct
         userdata.close()
-        return False
+        return False #Returns false if username or password are incorrect
 
 
-class SignUp():
+class SignUp(): #Signs up new users
 
     def __init__(self, fname, lname, username, password):
         self.username = username
@@ -31,21 +28,20 @@ class SignUp():
         self.fname = fname
         self.lname = lname
 
-    def CreateLogin(self):
+    def CreateLogin(self): #Creates data entries for new users
         userdata = open(os.path.join(os.getcwd(), 'User_Data.txt'), 'a')
-        userdata.write('\n' + self.username + ':' + self.password)
+        userdata.write('\n' + self.username + ':' + self.password) #Adds username and paassword to User_Data.txt
         userdata.close()
-        newuser = open(os.path.join(os.getcwd(), 'Users\\' + self.username + '.txt'), 'w+')
-        # input starting data for new user
+        newuser = open(os.path.join(os.getcwd(), 'Users\\' + self.username + '.txt'), 'w+') #Creates new txt file for users budget information
+        # input starting data for each new user
         newuser.close()
 
-    def CheckSignUp(self):
+    def CheckSignUp(self): #Checks if new users username is already used
         userdata = open(os.path.join(os.getcwd(), 'User_Data.txt'), 'a')
-        for line in open(os.path.join(os.getcwd(), 'User_Data.txt')):
+        for line in userdata:
             if self.username == (line.split(':'))[0]:
                 userdata.close()
-                return False
-            else:
-                userdata.close()
-                self.CreateLogin()
-                return True
+                return False #Returns false if username is taken
+        userdata.close()
+        self.CreateLogin()
+        return True #Returns true if username is available
